54e4212b295569f13713022c8a3075d96881d4f6
USER <user@USERnoMac-mini.local>
2024-03-12 20:20:04 +0900

    update

--- simple.cpp
+++ simple.cpp
@@ -3,7 +3,9 @@
 
 void kerokero1(){
   ELAPSE  elapse(__func__);
-  std::vector<int> vec = {1,2,3,4,5};
+  std::vector<int> vec = 
+{1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25};
+  std::vector<int> vec2(vec);
 }
 
 int main(){
